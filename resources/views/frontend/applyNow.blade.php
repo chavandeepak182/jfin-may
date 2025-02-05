0c9297084c823cbc24d04c43f275ece880753198
Vaibhav Saxena <shubhav260397@gmail.com>
2025-02-05 15:54:17 +0530

    Apply Loan Steps

--- resources/views/frontend/applyNow.blade.php
+++ resources/views/frontend/applyNow.blade.php
@@ -6,10 +6,10 @@
 <!-- Professional Details -->
 <div class="container-fluid contact bg-breadcrumb pt-5 mb-5" style="background: url(../theme/frontend/img/login.jpg);">
     <div class="container">
-        <div class="row text-center justify-content-center">
+        <div class="row text-center justify-content-center wow fadeInUp" data-wow-delay="0.2s">
             <div class="col-md-6 mb-5 bg-white rounded">
                 <div class="row align-items-center">
-                    <div class="col-md-6 p-0 wow fadeInLeft" data-wow-delay="0.2s">
+                    <div class="col-md-6 p-0">
                         <img src="{{ asset('theme') }}/frontend/img/apply-now.jpg" class="img-fluid w-100" style="border-radius: 10px 0px 0px 10px;" alt="Image">
                     </div>
 
@@ -29,10 +29,10 @@
                     </div>
                     @endif
 
-                    <div class="col-md-6 wow fadeInRight" data-wow-delay="0.4s">
+                    <div class="col-md-6">
                         <a href="/start_loan/1" class="btn btn-dark rounded-0 py-2 px-4 ms-3 flex-shrink-0">Start New Application</a> 
                         @if(Session::get('username'))
-                            <a href="/start_loan/2" class="btn btn-light rounded-0 py-2 px-4 ms-3 flex-shrink-0">Exiting Loans</a> 
+                            <a href="/start_loan/2" class="btn btn-light rounded-0 py-2 px-4 ms-3 mt-3 flex-shrink-0">Exiting Loans</a> 
                         @endif
                     </div>
                 </div>
