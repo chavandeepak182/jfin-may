d90235d95ee4c40d570cf9aac98c2afd63c23833
chavandeepak182 <chavandeepak182@gmail.com>
2026-01-03 19:58:22 +0530

    resolve conflicts

--- resources/views/leads/create.blade.php
+++ resources/views/leads/create.blade.php
@@ -24,7 +24,7 @@
         <div class="row">
             <div class="col-md-3">
                 <div class="form-group">
-                    <label>Full  *</label>
+                    <label>Full Name *</label>
                     <input type="text" name="name" class="form-control" required>
                 </div>
             </div>
