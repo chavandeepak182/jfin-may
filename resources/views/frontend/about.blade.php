5504d75ce05efc11243bffc299f4e05ed0796b7f
dhara-fs <dhara@jfstechnologies.com>
2026-01-08 19:35:14 +0530

    emi calculator updated

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -678,6 +678,34 @@ document.querySelectorAll('.service-btn').forEach(btn => {
                 </div>
             </div>
 
+            <!-- Nidhi Sonigra -->
+            <div class="col-lg-3 col-md-6" data-aos="fade-up">
+                <div class="team-card">
+                    <img src="{{ asset('theme') }}/frontend/img/team/nidhi.png" alt="Nidhi Sonigra">
+                    <ul class="social-link">
+                        <li><a href="#"><i class="bx bxl-linkedin-square"></i></a></li>
+                    </ul>
+                    <div class="content">
+                        <h3>Nidhi Sonigra</h3>
+                        <span>Manager - Admin</span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Lokesh Bhosale -->
+            <div class="col-lg-3 col-md-6" data-aos="fade-up">
+                <div class="team-card">
+                    <img src="{{ asset('theme') }}/frontend/img/team/lokesh.png" alt="Lokesh Bhosale">
+                    <ul class="social-link">
+                        <li><a href="#"><i class="bx bxl-linkedin-square"></i></a></li>
+                    </ul>
+                    <div class="content">
+                        <h3>Lokesh Bhosale</h3>
+                        <span>Sales Manager</span>
+                    </div>
+                </div>
+            </div>
+
             <!-- Prakash -->
             <div class="col-lg-3 col-md-6" data-aos="fade-up">
                 <div class="team-card">
@@ -705,35 +733,36 @@ document.querySelectorAll('.service-btn').forEach(btn => {
                     </div>
                 </div>
             </div>
-
-            <!-- Lokesh Bhosale -->
+            
+            <!-- Kevin Sunny -->
             <div class="col-lg-3 col-md-6" data-aos="fade-up">
                 <div class="team-card">
-                    <img src="{{ asset('theme') }}/frontend/img/team/lokesh.png" alt="Lokesh Bhosale">
+                    <img src="{{ asset('theme') }}/frontend/img/team/dumy.jpg" alt="Kevin Sunny">
                     <ul class="social-link">
                         <li><a href="#"><i class="bx bxl-linkedin-square"></i></a></li>
                     </ul>
                     <div class="content">
-                        <h3>Lokesh Bhosale</h3>
-                        <span>Sales Manager</span>
+                        <h3>Kevin Sunny</h3>
+                        <span>Sales Executive</span>
                     </div>
                 </div>
             </div>
 
-            <!-- Nidhi Sonigra -->
+            <!-- Avinash Bodke -->
             <div class="col-lg-3 col-md-6" data-aos="fade-up">
                 <div class="team-card">
-                    <img src="{{ asset('theme') }}/frontend/img/team/nidhi.png" alt="Nidhi Sonigra">
+                    <img src="{{ asset('theme') }}/frontend/img/team/dumy.jpg" alt="Avinash Bodke">
                     <ul class="social-link">
                         <li><a href="#"><i class="bx bxl-linkedin-square"></i></a></li>
                     </ul>
                     <div class="content">
-                        <h3>Nidhi Sonigra</h3>
-                        <span>Manager - Admin</span>
+                        <h3>Avinash Bodke</h3>
+                        <span>Senior Accountant</span>
                     </div>
                 </div>
             </div>
-
+            
+    
             <!-- Rushikesh Suryavanshi -->
             <div class="col-lg-3 col-md-6" data-aos="fade-up">
                 <div class="team-card">
@@ -772,6 +801,34 @@ document.querySelectorAll('.service-btn').forEach(btn => {
                     <div class="content">
                         <h3>Sonali Bhosale</h3>
                         <span>Executive Officer</span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Aasta Rokade -->
+            <div class="col-lg-3 col-md-6" data-aos="fade-up">
+                <div class="team-card">
+                    <img src="{{ asset('theme') }}/frontend/img/team/dumy.jpg" alt="Aasta Rokade">
+                    <ul class="social-link">
+                        <li><a href="#"><i class="bx bxl-linkedin-square"></i></a></li>
+                    </ul>
+                    <div class="content">
+                        <h3>Aasta Rokade</h3>
+                        <span>Executive Telecaller</span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Nikita Sanap -->
+            <div class="col-lg-3 col-md-6" data-aos="fade-up">
+                <div class="team-card">
+                    <img src="{{ asset('theme') }}/frontend/img/team/dumy.jpg" alt="Nikita Sanap">
+                    <ul class="social-link">
+                        <li><a href="#"><i class="bx bxl-linkedin-square"></i></a></li>
+                    </ul>
+                    <div class="content">
+                        <h3>Nikita Sanap</h3>
+                        <span>Executive Telecaller</span>
                     </div>
                 </div>
             </div>
